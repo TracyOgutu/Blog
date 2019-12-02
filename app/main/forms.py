0b5d9d72fe0497bc09d912482883c059df84deab
Tracy Ogutu <tracyogutu@gmail.com>
2019-12-03 00:56:01 +0300

    remove form

--- app/main/forms.py
+++ app/main/forms.py
@@ -2,21 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,RadioField
 from wtforms.validators import Required
 
-class PitchForm(FlaskForm):
-
-    title=StringField('Pitch title', validators=[Required()])
-    category=RadioField('Label',choices=[('investorpitch','investorpitch'),('productpitch','productpitch'),('interviewpitch','interviewpitch'),('marketpitch','marketpitch')],validators=[Required()])
-    description=TextAreaField('Enter your pitch', validators=[Required()])
-    submit=SubmitField('Submit')
 
 class CommentForm(FlaskForm):
     description=TextAreaField('Leave a comment')
-    submit=SubmitField()
-
-class UpvoteForm(FlaskForm):
-    submit=SubmitField()
-
-class DownvoteForm(FlaskForm):
     submit=SubmitField()
 
 class UpdateProfile(FlaskForm):
